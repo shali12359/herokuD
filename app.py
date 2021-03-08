81a91aefa0f3a9f72323de44076586be29ad3f7c
IT17186766_Prabath <it17186766@my.sliit.lk>
2021-03-08 14:55:33 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import flask
+from flask import jsonify
+from test import sayHello
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def predict():
+  sayHello('Shalitha')
+  return jsonify({ 'msg': 'Hello Android'})
+
+@app.route('/predict', methods = ['GET', 'POST'])
+def predictNew():
+  sayHello('Shalitha')
+  return jsonify({ 'msg': 'Hello Android 2'})
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=9696, debug=True)
