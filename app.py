ccbb5f2c09ed1743ebb2edda6466df63f232c379
IT17186766_Prabath <it17186766@my.sliit.lk>
2021-04-05 16:50:14 +0530

    modified API 3

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 from flask import jsonify
 from test import sayHello
+from Voice_identifying_service import voice_identifying_service
 
 app = flask.Flask(__name__)
 
@@ -13,6 +14,16 @@ def predict():
 def predictNew():
   sayHello('Shalitha')
   return jsonify({ 'msg': 'Hello Android 2'})
-	
+
+@app.route('/predictVerbal', methods= ['GET', 'POST'])
+def predict():
+  identify_voice = voice_identifying_service()
+
+  predicted_number = identify_voice.predict('222_1.wav')
+
+  voice_dataset = {'Keyword': predicted_number}
+
+  return jsonify(voice_dataset)
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=9696, debug=True)
