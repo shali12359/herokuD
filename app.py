8a83171ad7f8efcac0669a11cda22d1a89ec3e95
IT17186766_Prabath <it17186766@my.sliit.lk>
2021-04-05 16:32:46 +0530

    modified API

--- app.py
+++ app.py
@@ -16,28 +16,28 @@ def predictNew():
 
 @app.route("/predictVerbal", methods=["POST"])
 def predict():
-    # GET AUDIO FILE & SAVE
-    # audio = request.files['file']
-    # fileName = str(random.randint(0, 100000))
-    # audio.save(fileName)
+  # GET AUDIO FILE & SAVE
+  # audio = request.files['file']
+  # fileName = str(random.randint(0, 100000))
+  # audio.save(fileName)
 
-    # INVOKE SPOTTING SYSTEM
-    identify_voice = voice_identifying_service()
+  # INVOKE SPOTTING SYSTEM
+  identify_voice = voice_identifying_service()
 
-    # MAKE PREDICTION
-    # predicted_number = identify_voice.predict(fileName)
-    predicted_number = identify_voice.predict('222_1.wav')
+  # MAKE PREDICTION
+  # predicted_number = identify_voice.predict(fileName)
+  predicted_number = identify_voice.predict('222_1.wav')
 
-    # # REMOVE AUDIO FILE
-    # # os.remove(fileName)
+  # # REMOVE AUDIO FILE
+  # # os.remove(fileName)
 
-    # # SEND BACK PREDICTION
-    voice_dataset = {'Keyword': predicted_number}
+  # # SEND BACK PREDICTION
+  voice_dataset = {'Keyword': predicted_number}
 
-    print(predicted_number)
-    return jsonify(voice_dataset)
+  print(predicted_number)
+  return jsonify(voice_dataset)
 
-    # return "7"
+  # return "7"
 	
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=9696, debug=True)
