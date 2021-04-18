ab7863aa094128a859507e22b74c9318546dfa80
Prabath Shalitha <58390521+shali12359@users.noreply.github.com>
2021-04-18 11:25:16 +0530

    vebal removed

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ def predictNew():
   sayHello('Shalitha')
   return jsonify({ 'msg': 'Hello Android 2'})
 
-@app.route('/predictVerbal', methods= ['GET', 'POST'])
-def predictVerbal():
-  identify_voice = voice_identifying_service()
+# @app.route('/predictVerbal', methods= ['GET', 'POST'])
+# def predictVerbal():
+#   identify_voice = voice_identifying_service()
 
-  predicted_number = identify_voice.predict('635_225.wav')
+#   predicted_number = identify_voice.predict('635_225.wav')
 
-  voice_dataset = {'Keyword': predicted_number}
+#   voice_dataset = {'Keyword': predicted_number}
 
-  return jsonify(voice_dataset)
+#   return jsonify(voice_dataset)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=9696, debug=True)
